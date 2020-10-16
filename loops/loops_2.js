912ea3d5649bd52e5ef05f66392db21942b08c01
oliver olive <oo@olivers-MBP.attlocal.net>
2020-10-16 19:16:53 -0400

    refactoring code

--- loops/loops_2.js
+++ loops/loops_2.js
@@ -2,18 +2,18 @@
 // return false if there are not any common characters
 
 // first group
-const array1 = ['x','y','z'];
-const array2 = ['n','m','j'];
+const x = ['x','y','z'];
+const y = ['n','m','j'];
 
 // second group -- control group : designed to fail or return true
-const array3 = [1,2,3,4];
-const array4 = [1,3,5,6];
+const a = [1,2,3,4];
+const b = [1,3,5,6];
 
 //O(n^2)
 function array_smasher(array_1, array_2){
-  for (index1 = 0; index1 < array_1.length; index1++){
-    for (index2 = 0; index2 < array_2.length; index2++) {
-      if (array_1[index1] === array_2[index2]){
+  for (i = 0; i < array_1.length; i++){
+    for (j = 0; j < array_2.length; j++) {
+      if (array_1[i] === array_2[j]){
         return true;
       }
     }
